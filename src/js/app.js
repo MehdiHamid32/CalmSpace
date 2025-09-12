7016612bb4602f3d58fbc634eb1f13cbab09480c
MehdiHamid <mehdi.hamid0032@gmail.com>
2025-09-12 09:30:24 +0330

    Finish CalmSpace Project

---
+++ src/js/app.js
@@ -0,0 +1,72 @@
+"use strict";
+
+let $ = document;
+
+let startSection = $.getElementById('startSection');
+let content = $.getElementById('content');
+let startButton = $.getElementById('startButton');
+let audio = $.getElementById('audio');
+let clockText = $.getElementById('clockText');
+let dateSection = $.getElementById('dateText');
+
+var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+setTimeout(() => {
+    startButton.classList.remove('opacity-0');
+}, 1000);
+
+const showTime = () => {
+    let date = new Date();
+
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = (h >= 12) ? "PM" : "AM";
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    clockText.textContent = `${h} : ${m} : ${s} ${session}`;
+
+    let dayName = week[date.getDay()];
+    dateSection.textContent = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}, ${dayName}`;
+}
+
+
+const startButtonHandler = () => {
+    audio.play();
+
+    startButton.classList.add('opacity-0');
+
+    setTimeout(() => {
+        startButton.innerText = "Started ...";
+    }, 100);
+
+    setTimeout(() => {
+        startButton.classList.remove('opacity-0');
+    }, 500);
+
+    setTimeout(() => {
+        startButton.classList.add('opacity-0');
+    }, 1500);
+
+    setTimeout(() => {
+        startSection.classList.add('hidden');
+
+        showTime();
+        setInterval(showTime, 1000);
+
+        content.classList.remove('hidden');
+    }, 2000);
+}
+
+showTime();
+setInterval(showTime, 1000);
+
+startButton.addEventListener('click', startButtonHandler);
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/src/assets/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
